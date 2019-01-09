5fb5849f32fba558489b1247eb71ea234395b7e1
Damian Stopa <stopa.damian997@gmail.com>
2019-01-09 11:10:26 +0100

    statki + plansza

---
+++ src/statki/Plansza.java
@@ -0,0 +1,63 @@
+package statki;
+
+import static statki.Statki.grid;
+import static statki.Statki.numCols;
+
+
+public class Plansza {
+
+    public void createOceanMap() {
+        //First section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+                System.out.print(i);
+        System.out.println();
+
+        //Middle section of Ocean Map
+        for(int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = " ";
+                if (j == 0)
+                    System.out.print(i + "|" + grid[i][j]);
+                else if (j == grid[i].length - 1)
+                    System.out.print(grid[i][j] + "|" + i);
+                else
+                    System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+
+        //Last section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+    
+    public void printOceanMap(){
+        System.out.println();
+        //First section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+
+        //Middle section of Ocean Map
+        for(int x = 0; x < grid.length; x++) {
+            System.out.print(x + "|");
+
+            for (int y = 0; y < grid[x].length; y++){
+                System.out.print(grid[x][y]);
+            }
+
+            System.out.println("|" + x);
+        }
+
+        //Last section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+    
+}
